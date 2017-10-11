9197c32cb11afd07b11633877194e2bba70733a0
Francisco Azevedo <francisco.n.azevedo@gmail.com>
2017-10-11 14:23:09 +0100

    Changed Reg/ Uploaded REL

--- Lab1/regularization.m
+++ Lab1/regularization.m
@@ -7,24 +7,30 @@ clc;
 close all;
 load('data3.mat')
 
-%Create a beta matrix
-P = 3;
-
 %Compute the cost function (compare with previously obtained LS in code1.m
 %LS = ((X*(beta)) - Y).^2;
 
 %Lasso regression plot with FitInfo.Lambda taken from lasso(X,Y)
 [B, FitInfo] = lasso(X,Y);
 lassoPlot(B, FitInfo,'PlotType', 'Lambda', 'XScale', 'log');
-%FitInfo.Lambda
-%lassoPlot(B, FitInfo,'PlotType', 'Lambda', 'XScale', 'log');
+
 FitInfo.Lambda(60)
-Blasso=B(:,60);
-Y_0=Blasso(1)*X(:,1)+Blasso(2)*X(:,2)+Blasso(3)*X(:,3);
-figure();
-%hold on;
-plot(Y_0);
+Blasso = B(:,60);
+
+
+
+
+%Alinha 4. -> Plot Y and fit to data. Compute cost with and w/o regula.
+Y_0 = Blasso(1)*X(:,1) + Blasso(2)*X(:,2) + Blasso(3)*X(:,3);
+
+%Calculate cost with and without regularization
+cost_no_lambda = 1;
+cost_lambda = sum(((X*(Blasso)) - Y).^2);
+
 %plot 2o grafico
+figure();
+plot(Y_0);
+
 
 %Alinha 5. -> Ridge regression instead of Lasso regression
 K = 1;
