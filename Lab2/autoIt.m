14671159ec366bf20ec86f171eab59e1936bc01d
Duarte Dias <DuartexDias@gmail.com>
2017-10-17 13:00:31 +0100

    2.2.1 script

--- Lab2/autoIt.m
+++ Lab2/autoIt.m
@@ -1,16 +1,12 @@
 %Este script gera qualquer tabela do lab 2 num ficheiro excel
 %Para obter tabelas diferentes basta mudar onde diz "mudar esta funcao"
 %ToDo: aprender a usar alias para os nomes das funções
-columns = [.001 .01 .03 .1 .3 1 3 0;
-            .01 .03 .1 .3 1 3 0 0;
-            .003 .01 .03 .1 .3 1 3 10;
-            .001 .01 .1 1 10 0 0 0];
-rows = [.5 1 2 5 0 0;
-        2 20 0 0 0 0;
-        0 .5 .7 .9 .95 0;
-        0 .5 .7 .9 .95 .99];
 
 
+%Exercise 2.1.1 - prints to an excel file
+clear;
+tablesvalues;
+quad1ini;
 for i1 = 1:6
     for i2 = 1:8          %mudar esta funcao
         result1(i2, i1) = quad1mod(rows(1,i1), columns(1, i2));
@@ -19,6 +15,19 @@ end
 
 filename = 'table1.xlsx';
 xlswrite(filename, result1, 'Range','I4');
-result1
+
+%Exercise 2.2.1 - prints all results on the console
+clear;
+tablesvalues;
+quad2ini;
+anim = 0;
+for i1 = 1:6
+    for i2 = 1:8          %mudar esta funcao
+        a = rows(2, i1);
+        eta = columns(2, i2);
+        quad2
+    end
+end
+
 
 
