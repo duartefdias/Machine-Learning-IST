372f0c84484bd6959f26d4637f03393ec432d71c
Duarte Dias <DuartexDias@gmail.com>
2017-10-28 17:58:51 +0100

    Finished all the matlab stuff for part2

--- Lab3/TwotwoSetup.m
+++ Lab3/TwotwoSetup.m
@@ -1,7 +1,18 @@
-net = fitnet(40);
-net.layers{2}.transferFcn='purelin';
+net = fitnet(40);  %single hidden layer with 0 units
+net.layers{2}.transferFcn='purelin'; %setting the activation of the output layer to linear
 
-net.performFcn='mse';
+net.performFcn='mse'; %defining cost function = mean squared error
 net.trainParam.show=10000; % # of epochs in display
 net.trainParam.epochs=10000;% max epochs
 net.trainParam.goal=0.005; % training goal
+
+%Start Neural Network Toolbox GUI
+%nnstart
+
+%Training without validation set
+net.divideParam.trainRatio = 1;
+net.divideParam.valRatio   = 0;
+net.divideParam.testRatio  = 0;
+
+%Train neural network
+[net,tr]=train(net,X,T)
