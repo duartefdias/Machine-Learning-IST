cc3e71a49003e77b72a793c3d505bb705b0e5148
Francisco Azevedo <francisco.n.azevedo@gmail.com>
2017-10-31 18:38:49 +0000

    Made Half of Part 1

---
+++ Lab3/Two_one_setup.m
@@ -0,0 +1,19 @@
+net = patternnet([15]);
+net.performFcn = 'mse';
+net.layers{1}.transferFcn = 'tansig';
+net.layers{2}.transferFcn = 'tansig';
+
+net.divideFcn = 'divideind';
+net.divideParam.trainInd = 1:400;
+net.divideParam.trainInd = 401:560;
+
+
+%Train neural network
+net.trainFcn = 'traingdx'
+net.trainParam.lr = 3; %1.1 for mementum 3 for step
+net.trainParam.mc = 0.7; %0.6 for mementu 0.55 for step
+net.trainParam.show = 10000;
+net.trainParam.epochs = 10000;
+net.trainParam.goal = 0.05;
+
+[net,tr]=train(net,X,T);
